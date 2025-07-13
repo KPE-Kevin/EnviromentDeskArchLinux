10ab5577bace8e03c32fe8986ac707e13cb37e98
Kevin Perez Escamilla <kpeitics@gmail.com>
2025-07-13 16:47:56 -0600

    configuraciones de kitty

--- install.sh
+++ install.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-echo 'Hola bienvenido a la instacion del entorno de trabajo'
-echo ' '
+#echo 'Hola bienvenido a la instacion del entorno de trabajo'
+#echo ' '
 
 # Actualizar sistema
-echo 'Actualizando sistema'
+#echo 'Actualizando sistema'
+#echo ''
 #sudo pacman -Sy
 #sudo pacman -Syu
 
+#Crear Carpetas
+
+#echo 'Crear carpetas de systema y de respaldo'
+#mkdir respaldo
+#mkdir ~/Documents                                                                                                                                                                                                 
+#mkdir ~/Proyects                                                                                                                                                                                                  
+#mkdir ~/Images                                                                                                                                                                                                    
+#mkdir ~/
+
+#~
+
 # Instalar paquetes
-echo 'Instalacion de paquetes'
-echo ' '
+#echo 'Instalacion de paquetes'
+#echo ' '
 #sudo pacman -S nvim fastfetch btop
 #sudo pacman -S --needed git base-devel
 #git clone https://aur.archlinux.org/yay.git
@@ -21,35 +33,31 @@ echo ' '
 #sudo pacman -S jdk21-openjdk maven
 #sudo pacman -S netbeans nodejs
 #sudo pacman -S ttf-nerd-fonts-symbols
+#yay -S hyprlock-git
+
 
 
 #Respaldo de archivos orignales
-echo "Configuraciones de paquetes"
-echo " "
+#echo "Configuraciones de paquetes"
+#echo " "
 #Configuracion para Network Manager
 #sudo systemctl enable NetworkManager
 #sudo systemctl start NetworkManager
 #rfkill list
 #sudo rfkill unblock all
 
+# Respaldos de archivos de configuración de los aplicativos
+#cp ~/.config/hypr/hyprland.conf ./respaldo/hypr
+#cp -r /etc/xdg/waybar/. ./respaldo/waybar
+#cp /usr/lib/sddm/sddm.conf.d/default.conf ./respaldo/sddm
 
-
-#mkdir respaldo
-
-#cp ~/.config/hypr/hyprland.conf ./respaldo
-#cp -r /etc/xdg/waybar/. ./respaldo
-
-#Crear Carpetas
-#mkdir ~/Documents                                                                                                                                                                                                 
-#mkdir ~/Proyects                                                                                                                                                                                                  
-#mkdir ~/Images                                                                                                                                                                                                    
-#mkdir ~/
-
-#~
-
+# Direccion de temas de sddm cd /usr/share/sddm/themes/
+# Direccion de configuracion de sddm /usr/lib/sddm/sddm.conf.d/default.conf 
 
 #Copiado de configuraiones del entorno de escritorio
 
-#cp ./config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
-sudo cp ./config/waybar/config.jsonc /etc/xdg/waybar/config.jsonc
-#cp ./config/hyprland.conf ~/.config/hypr/hyprland.conf
+cp ./config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+#sudo cp ./config/waybar/config.jsonc /etc/xdg/waybar/config.jsonc
+#sudo cp ./config/waybar/style.css /etc/xdg/waybar/style.css
+#cp ./config/hyprlock/hyprlock.conf ~/.config/hypr/hyprlock.conf
+#sudo cp ./config/sddm/default.conf /usr/lib/sddm/sddm.conf.d/default.conf 
